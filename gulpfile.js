9eded4a13ad07f5724b4963ebe7e76ba1fa4e8df
I. Chupryna <ichupryna@gmail.com>
2016-11-25 16:43:34 +0200

    Adding karma tests

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ var gulp = require('gulp'),
 
 var files = ['js/**/*.js'];
 
+
+//neet to get jquery.min, angular, angular-route, angular-mocks, bootstrap
+var karmaFiles = [];
+var spec = 'js/spec/*.js';
+var bundle = 'bin/js/**/*.js';
+
 gulp.task('scripts', function(){
   gulp.src(files)
     .pipe($.babel({
@@ -22,6 +28,20 @@ gulp.task('validate', function () {
     gulp.src(files)
         .pipe($.jshint())
         .pipe($.jshint.reporter(stylish))
+})
+
+gulp.task('tests', function (done) {
+
+    var testFiles = [spec, bundle];
+    testFiles = karmaFiles.concat(testFiles);
+
+    new karma.Server({
+        configFile: __dirname + '/karma.conf.js',
+        files : testFiles,
+        singleRun: true
+    }, function () {
+        done();
+    }).start();
 })
 
 gulp.task('default', ['scripts', 'validate'], function() {
